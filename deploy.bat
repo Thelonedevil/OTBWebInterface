50a03306daf5b99c0ad7a23f10b248353c3ba2ba
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2015-07-02 17:52:22 +0100

    Changed type variable to repo to avoid conflicts
    Signed-off-by: Justin Wiblin <justin_wiblin@hotmail.co.uk>

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 @echo off
-set "type=%1"
+set "repo=%1"
 set "password=%2"
 call bundle exec rake assets:precompile
 call bundle exec warble war
 echo ^<settings^>^<servers^>^<server^>^<id^>snapshots^</id^>^<username^>deployment^</username^>^<password^>%password%^</password^>^</server^>^<server^>^<id^>release^</id^>^<username^>deployment^</username^>^<password^>%password%^</password^>^</server^>^</servers^>^</settings^> > settings.xml
-call mvn deploy:deploy-file -Dfile=OTBWebInterface.war -DpomFile=pom.xml -DrepositoryId=%type% -Durl=http://ts.tldcode.uk:8081/nexus/content/repositories/%type%/ --settings settings.xml
+call mvn deploy:deploy-file -Dfile=OTBWebInterface.war -DpomFile=pom.xml -DrepositoryId=%repo% -Durl=http://ts.tldcode.uk:8081/nexus/content/repositories/%repo%/ --settings settings.xml
 pause
